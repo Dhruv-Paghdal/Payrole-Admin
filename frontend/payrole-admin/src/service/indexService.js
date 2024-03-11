e732bda603b4fcbe2f643041ade7d45400b66137
Dhruv Paghdal <85886757+Dhruv-Paghdal@users.noreply.github.com>
2024-03-11 15:54:33 +0530

    Update indexService.js

--- frontend/payrole-admin/src/service/indexService.js
+++ frontend/payrole-admin/src/service/indexService.js
@@ -8,7 +8,7 @@ export const login = async(payload) => {
         return {success: true, status: res.data.status, data: res.data.data , message: res.data.message}
     } catch (error) {
         if (!error.response) {
-            return {success: false, status: 503, message: 'Error: Network Error', data: ""}
+            return {success: false, status: 503, message: error, data: ""}
         } else {
             return {success: false, status: error.response.data.status, message: error.response.data.message, data: ""}
         }
